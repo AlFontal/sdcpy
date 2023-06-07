88b5858c544108a6d13add8f3b1e7e6d25f8f313
Alejandro Fontal <alejandrofontal92@gmail.com>
2023-06-07 17:40:51 +0200

    feat: add basic tests (#1)
    Very basic at this point, but it's a start.

--- tests/test_sdcpy.py
+++ tests/test_sdcpy.py
@@ -4,22 +4,22 @@
 """Tests for `sdcpy` package."""
 
 import pytest
-
-
+import numpy as np
+import itertools
 from sdcpy.scale_dependent_correlation import SDCAnalysis
 
+ts1 = np.random.rand(100)
+ts2 = np.random.rand(100)
+fragment_sizes = [10, 50, 70]
+methods = ["pearson", "spearman"]
+params = list(itertools.product(fragment_sizes, methods)) 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+@pytest.mark.parametrize('fragment_size,method', params)
+def test_sdc_analysis(fragment_size, method):
+    sdc = SDCAnalysis(ts1, ts2, fragment_size=fragment_size, method=method)
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    assert sdc.fragment_size == fragment_size
+    assert sdc.sdc_df.shape[0] == (len(ts1) - fragment_size) * (len(ts2) - fragment_size)
 
+    sdc.combi_plot()  # Just checking that the figure can be generated
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
