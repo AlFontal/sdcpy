b73fa94ae2c370c2f2f9602c7537905ef7e5b232
Alejandro Fontal <alejandro.fontal.92@gmail.com>
2025-12-22 21:01:40 +0100

    chore: unify release to v0.7.0

--- sdcpy/__init__.py
+++ sdcpy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Alejandro Fontal"""
 __email__ = "alejandrofontal92@gmail.com"
-__version__ = "0.5.2"
+__version__ = "0.7.0"
 
 # Public API
 from sdcpy.core import compute_sdc, generate_correlation_map, shuffle_along_axis
